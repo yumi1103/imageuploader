871576ff4fd153e853835a8d8ed7f9a27927200d
yumi1103 <tanisi.1103@gmail.com>
2020-11-02 23:25:16 +0900

    add design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1 className="App-title">ImageUploader</h1>
+      <div className="Drop-area">
+        <p>ここにファイルをドラッグ&ドロップしてください</p>
+      </div>
+      <div className="Upload-area">
+        <p>xxx.jpg</p>
+        <button className="Upload-button">アップロード</button>
+      </div>
     </div>
   );
 }
